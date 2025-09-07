e725f31ace123dccd683769b3fc96a787f813b78
Flareventures <flareventures19@gmail.com>
2025-09-06 19:18:47 -0500

    Update firebaseConfig.sample.ts

--- src/config/firebaseConfig.sample.ts
+++ src/config/firebaseConfig.sample.ts
@@ -3,12 +3,12 @@
 // Option B: export already-initialized { app, db } (Firestore).
 
 export const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_MSG_SENDER',
-  appId: 'YOUR_APP_ID',
+  apiKey: 'AIzaSyDN0lPIkSuPybu5tFEvnRqzryPCsP5JU0A',
+  authDomain: 'tame-b2463.firebaseapp.com',
+  projectId: 'tame-b2463',
+  storageBucket: 'tame-b2463.firebasestorage.app',
+  messagingSenderId: '906027510456',
+  appId: '1:906027510456:web:0de936e84cea8ff039624d',
 };
 
 // Or, alternatively:
